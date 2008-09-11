71cc060f7ec7ab93cc9bb51fa18e2f3e0d2086bc
lucascs <lucascs@767ad012-344d-0410-8a24-9b9aef242d6b>
2008-09-11 19:56:05 +0000

    tests green!

--- src/test/java/br/com/caelum/seleniumdsl/test/integration/GeneralTest.java
+++ src/test/java/br/com/caelum/seleniumdsl/test/integration/GeneralTest.java
@@ -1,125 +1,81 @@
 package br.com.caelum.seleniumdsl.test.integration;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.server.SeleniumServer;
 
-import br.com.caelum.seleniumdsl.Browser;
-import br.com.caelum.seleniumdsl.DefaultBrowser;
 import br.com.caelum.seleniumdsl.Form;
 import br.com.caelum.seleniumdsl.Page;
 import br.com.caelum.seleniumdsl.table.Table;
 
-import com.thoughtworks.selenium.DefaultSelenium;
-import com.thoughtworks.selenium.Selenium;
-import com.thoughtworks.selenium.SeleniumLogLevels;
+public class GeneralTest extends SeleniumTestCase {
 
-public class GeneralTest {
-
-	private static Selenium selenium;
-
-	private Browser browser;
-
-	@BeforeClass
-	public static void beforeStartup() {
-		selenium = new DefaultSelenium("localhost", SeleniumServer.getDefaultPort(), "*firefox",
-				"http://localhost:8080/");
-		selenium.start();
-		selenium.setContext("Selenium DSL testing");
-		selenium.setBrowserLogLevel(SeleniumLogLevels.DEBUG);
-	}
-
-	@AfterClass
-	public static void afterShutdown() {
-		selenium.stop();
-	}
-
+	@Override
 	@Before
 	public void setUp() {
-		browser = new DefaultBrowser(selenium);
-		browser.open("seleniumdsl/mypage.html");
+		super.setUp();
+		browser.open("mypage.jsp");
 	}
 
 	@Test
 	public void testTableRowCount() {
-		Table table = browser.currentPage()
-				.table("table");
+		Table table = browser.currentPage().table("table");
 		Assert.assertEquals(table.getRowCount(), 3);
 	}
 
 	@Test
 	public void testTableColCount() {
-		Table table = browser.currentPage()
-				.table("table");
+		Table table = browser.currentPage().table("table");
 		Assert.assertEquals(table.getColCount(), 2);
 	}
 
 	@Test
 	public void testTableHeaderLabel() {
-		Table table = browser.currentPage()
-				.table("table");
+		Table table = browser.currentPage().table("table");
 		// indexes start at 1
-		Assert.assertEquals(table.header()
-				.cell(1)
-				.headerValue(), "First Column");
+		Assert.assertEquals(table.header().cell(1).headerValue(), "First Column");
 	}
 
 	@Test
 	public void testTableContent() {
-		Table table = browser.currentPage()
-				.table("table");
+		Table table = browser.currentPage().table("table");
 		// second row, column named "First Column"
 		// value() returns null if the cell isn't found
-		Assert.assertEquals(table.cell(1, "First Column")
-				.value(), "cell_1_1");
+		Assert.assertEquals(table.cell(1, "First Column").value(), "cell_1_1");
 		// or
-		Assert.assertTrue(table.cell(1, "First Column")
-				.contains("cell_1_1"));
+		Assert.assertTrue(table.cell(1, "First Column").contains("cell_1_1"));
 	}
 
 	@Test
 	public void testLogin() {
-		Form form = browser.currentPage()
-				.form("form");
-		form.field("login")
-				.type("myLogin");
-		form.field("password")
-				.type("myPassword");
+		Form form = browser.currentPage().form("form");
+		form.field("login").type("myLogin");
+		form.field("password").type("myPassword");
 		form.check("remember");
 
 		form.submit();
 
 		// logged successfuly
-		Assert.assertEquals("Main Page", browser.currentPage()
-				.title());
+		Assert.assertEquals("Main Page", browser.currentPage().title());
 	}
 
 	@Test
 	public void testInvalidLogin() {
-		Form form = browser.currentPage()
-				.form("form");
-		form.field("login")
-				.type("invalid_login");
-		form.field("password")
-				.type("invalid_password");
+		Form form = browser.currentPage().form("form");
+		form.field("login").type("invalid_login");
+		form.field("password").type("invalid_password");
 
 		form.submit();
 
 		Page page = browser.currentPage();
 
 		Assert.assertEquals("Login", page.title());
-		Assert.assertTrue(page.div("errors")
-				.contains("Invalid login!"));
+		Assert.assertTrue(page.div("errors").contains("Invalid login!"));
 
-		Assert.assertTrue(form.isChecked("rememeber"));
+		Assert.assertTrue(form.isChecked("remember"));
 		// ensures that the form remains with the previously typed login
-		Assert.assertEquals(form.field("login")
-				.content(), "invalid_login");
+		Assert.assertEquals(form.field("login").content(), "invalid_login");
 		// or
-		Assert.assertTrue(form.field("login")
-				.contains("invalid_login"));
+		Assert.assertTrue(form.field("login").contains("invalid_login"));
 	}
 }
---
+++ src/test/resources/mypage.jsp
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+    	<script type="text/javascript">
+    		function validate(form) {
+				if (document.getElementById('login').value == 'myLogin'
+					&& document.getElementById('password').value == 'myPassword')
+					return true;
+				else {
+					var input = document.createElement('input');
+					input.type = 'hidden';
+					input.name = 'errors';
+					input.value = 'Invalid login!';
+					form.appendChild(input);
+					form.action = '';
+					return true;
+				} 
+    		}
+    	</script>
+        <table id="table">
+            <thead>
+            	<tr>
+                	<th>First Column</th>
+                	<th>Second Column</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>cell_1_1</td>
+                    <td>cell_1_2</td>
+                </tr>
+                <tr>
+                    <td>cell_2_1</td>
+                    <td>cell_2_2</td>
+                </tr>
+            </tbody>
+        </table>
+    
+        <div id="errors">
+            <!-- populated on invalid login --> 
+			${param['errors'] }
+        </div>
+        <form id="form" action="main.html" onsubmit="return validate(this)" >
+            Login: <input id="login" type="text" name="login" value="${param['remember']? param['login']:'' }"/><br/>
+            Password: <input id="password" type="password" name="password" /><br/>
+            Remember? <input type="checkbox" name="remember" value="true" checked=${param['remember'] }/>
+        </form>
+    </body>
+</html>
